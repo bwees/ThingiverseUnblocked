15f1801cb3570719e078052526b700cee60c7143
bwees <brandonwees@gmail.com>
2022-02-22 16:17:11 -0500

    added download all files support

--- fixbuttons.js
+++ fixbuttons.js
@@ -11,14 +11,37 @@ function updateButtons(delay = true) {
             var button = buttons[i]
             var parent = button.parentElement
             if (parent.className.includes("ThingFile__download")) {
-                parent.className = "ThingFile__download"
                 button.innerHTML = "Download"
                 parent.href = fileList[fileIndex]
                 fileIndex++
             }
         }
     }
+
+    // Fix download all zip file button
+    var buttons = document.getElementsByClassName("button")
+    // for each buttons, if the parent elemnt has class name that contains "ThingFile", then print "found"
+    var fileIndex = 0
+    for (var i = 0; i < buttons.length; i++) {
+        var button = buttons[i]
+        var parent = button.parentElement
+        if (parent.className.includes("SidebarMenu__download")) {
+            button.innerHTML = "Download All Files"
+            parent.href = getZIPURL()
+            fileIndex++
+        }
+    }
 }
+
+function getZIPURL() {
+    //get link
+    var url = window.location.href
+
+    // split url into parts by "/"
+    var urlParts = url.split("/")
+    return urlParts[0] + "//" + urlParts[1] + urlParts[2] + "/" + urlParts[3] + "/zip"
+}
+
 
 if (fileList) {
     updateButtons(false);
